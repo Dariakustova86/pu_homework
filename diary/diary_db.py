30bbde44e769c43ca5cc6606336635fa8f151b36
Dariakustova86 <dashakustova86@gmail.com>
2018-04-26 22:50:45 +0300

    'correction'

--- diary/diary_db.py
+++ diary/diary_db.py
@@ -4,9 +4,9 @@ cursor = conn.cursor()
 sql = '''
 	CREATE TABLE IF NOT EXISTS diary (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL DEFAULT,
-		task_text TEXT NOT NULL DEFAULT, 
-		execution_time NOT NULL DEFAULT
+		name NOT NULL,
+		task_text NOT NULL,
+		time NOT NULL
 	)
 '''
 cursor.execute(sql)
