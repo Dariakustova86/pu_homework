35f488eca8739a45bb779adc5418872e61e117a6
Dariakustova86 <dashakustova86@gmail.com>
2018-05-18 07:01:26 +0300

    bug_fix

--- task_decorator_login_required.py
+++ task_decorator_login_required.py
@@ -1,14 +1,26 @@
+import hashlib
+
+def make_token(username, password):
+    s = username + password
+    return hashlib.md5(s.encode()).hexdigest()
+	
+user_aut == False
+
 def login_required(func):
-	def wrapper(*args, **kwargs):
-		def token_txt(read):
-			with open('token.txt') as f:
-				read = f.read()
-		for i in range(3):
-			username = input()
-			password = input()
-			test = make_token(token_txt(username, password))
-			if test == True:
-				break
-			return read 
-		return func(*args, **kwargs) if test else None
-	return wrapper
+    def wrapper(*args, **kwargs):
+        global user_aut
+        if user_aut == False:
+            i = 0
+            while i < 3 :
+                user = input()
+                password = input()
+                with open('token.txt') as token:
+                    if make_token(user, password) != token.read():
+                        i += 1
+                    else:
+                        user_aut == True
+                        return func(*args, **kwargs)
+            return None
+        else:
+            return func(*args, **kwargs)
+    return wrapper
