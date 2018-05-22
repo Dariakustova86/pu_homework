c3c64c82ca1a313d517a4bcdb451eb366fa9e2de
Dariakustova86 <38354320+Dariakustova86@users.noreply.github.com>
2018-05-22 15:35:21 +0300

    update

--- task_oop_2.py
+++ task_oop_2.py
@@ -1,26 +1,26 @@
 class Courses(object):
 	def __init__(self, date_start, duration, specialization, location, number_group):
-				
-	def add_group(Group):
-	
-	def get_date_end(self, date_start, duration):
-		date_end = date_start + duration
-		
-		
-class Group(object):
-	def __init__(self, number_student_min, number_student_max, knowledge_level):
 	
 	def add_teacher(Teacher):
 	
-	def add_students(Student):
+	def get_date_end(self, date_start, duration):
+		date_end = date_start + duration
 	
 	
 class Teacher(object):
 	def __init__(self, name, age, specialization, experience):
 	
+	def add_group(Group):
+	
 	def check_hw(self):
 	
 	def logging(self):
+		
+class Group(object):
+	def __init__(self, number_student_min, number_student_max, knowledge_level):
+	
+	def add_students(Student):
+
 	
 class Student(object):
 	def __init__(self, name, age, knowledge_level, specialization):
